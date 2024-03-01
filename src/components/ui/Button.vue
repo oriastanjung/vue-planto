8aceb1247cb28da0deebe735d2742b0e862a1902
oriastanjung <oriastan999@gmail.com>
2024-03-01 23:43:23 +0700

    finish hero

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,6 +1,3 @@
-<script setup>
-
-</script>
 
 <template>
     <button class="text-center lg:py-3 py-2 px-6 lg:px-14 bg-transparent border border-white rounded-xl lg:text-2xl text-lg font-normal">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
-<script setup >
-import TheWelcome from '../components/TheWelcome.vue'
+<script setup>
+
 import Navbar from "../components/layout/Navbar.vue"
 import Hero from "../components/organism/Hero.vue"
 </script>
 
 <template>
   <Navbar />
-  <main class="bg-[#1B2316] text-white w-full relative top-0 min-h-screen" >
+  <main class="bg-[#1B2316] text-white w-full relative top-0 min-h-screen">
     <div class="relative top-0"><img class="absolute top-0 z-0 h-2xl 2xl:w-full" src="/background.png" alt=""></div>
     <Hero />
   </main>
